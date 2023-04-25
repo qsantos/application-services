6475bd801799b89ba9c7fb08697c1244d4b61525
Ben Dean-Kawamura <bdk@mozilla.com>
2023-04-25 15:24:48 -0400

    Adding extra links to the release JSON file
    I think this will simplify consuming the build slightly.

--- taskcluster/scripts/generate-release-json.py
+++ taskcluster/scripts/generate-release-json.py
@@ -5,6 +5,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
+from urllib.parse import quote_plus
 import argparse
 import json
 import os
@@ -13,12 +14,30 @@ def main():
     args = parse_args()
     dump_json(args)
 
+def indexed_artifact_url(index_name, filename):
+    return 'https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/{}/artifacts/public{}'.format(
+            index_name, quote_plus(f"/build/{filename}"))
+
 def dump_json(args):
     data = {
         'version': args.version,
         'channel': args.maven_channel,
         'commit': os.environ['APPSERVICES_HEAD_REV'],
+        'nimbus-fml.zip': indexed_artifact_url(
+            f'project.application-services.v2.nimbus-fml.{args.version}', 'nimbus-fml.zip'),
+        'nimbus-fml.sha256': indexed_artifact_url(
+            f'project.application-services.v2.nimbus-fml.{args.version}', 'nimbus-fml.sha256'),
+        'FocusRustComponents.xcframework.zip': indexed_artifact_url(
+            f'project.application-services.v2.swift.{args.version}',
+            'FocusRustComponents.xcframework.zip'),
+        'MozillaRustComponents.xcframework.zip': indexed_artifact_url(
+            f'project.application-services.v2.swift.{args.version}',
+            'MozillaRustComponents.xcframework.zip'),
+        'swift-components.tar.xz': indexed_artifact_url(
+            f'project.application-services.v2.swift.{args.version}',
+            'swift-components.tar.xz'),
     }
+
     dir = os.path.dirname(args.path)
     if not os.path.exists(dir):
         os.makedirs(dir)
